575621abac0f3fdbb6f1f1de6a70c38d9d21adb8
Sandesh <sandesh@lumosys.in>
2025-03-27 17:35:09 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <footer className='w-screen bg-violet-300 py-4 text-black'>
         <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
-        <p className='text-center text-sm md:text-left'>
+        <p className='text-center text-sm md:text-left hover:text-white'>
             &copy; Sandesh 2025. All rights reserved
         </p>
 
@@ -31,7 +31,7 @@ const Footer = () => {
         </div>
         <a
           href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
+          className="text-center text-sm font-light hover:underline md:text-right hover:text-white"
         >
           Privacy Policy
         </a>
